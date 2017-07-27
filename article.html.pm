731a868d2143b91d3b92135bb86b6ae56561dc42
Chas Threlkeld <chathrel@indiana.edu>
2017-07-27 10:43:13 -0500

    Tutorial 3 begun

---
+++ article.html.pm
@@ -0,0 +1,5 @@
+#lang pollen
+
+◊(define (strong word) "BOOM")
+
+I am so excited to attend ◊em{RacketCon ◊strong{this} year}.
--- hello.html.pm
+++
@@ -1,7 +0,0 @@
-#lang pollen
-
-◊items{
-    ◊item{◊link["uptown.html"]{Mean Street}}
-    ◊item{◊link["downtown.html"]{Panama}}
-    ◊item{◊link["margin.html"]{Hear About It Later}}
-}
